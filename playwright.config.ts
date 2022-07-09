35952195b436ab93a63831b37fedcf70b3d7b211
David Peng <davipondev@gmail.com>
2022-07-09 10:10:11 +0800

    Upgrade SvelteKit to the latest version (#16)
    * chore(bump): bumpped sveltekit to latest version, add and set up vite.config.js
    
    * test(snapshot): add snapshot under windows

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,7 +3,7 @@ import type { PlaywrightTestConfig } from '@playwright/test';
 const config: PlaywrightTestConfig = {
 	webServer: {
 		command: 'pnpm build && pnpm preview',
-		port: 3000
+		port: 4173
 	}
 };
 
