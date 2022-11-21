6043668a7449eefcdd2c75bdf7415bd86ce0bade
David Peng <davipondev@gmail.com>
2022-11-21 18:05:00 +0800

    ci(git-hooks): remove git hooks like husky & commitizen (#47)
    * ci(git-hooks): remove git hooks like husky & commitizen
    
    * test(playwright): add reuseExistingServer config
    
    * chore: bumped packages
    
    * ci: remove husky install in scripts

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,7 +3,8 @@ import type { PlaywrightTestConfig } from '@playwright/test';
 const config: PlaywrightTestConfig = {
 	webServer: {
 		command: 'pnpm build && pnpm preview',
-		port: 4173
+		port: 4173,
+		reuseExistingServer: true
 	}
 };
 
